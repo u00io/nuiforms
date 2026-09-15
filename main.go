77bc3538497d8bef36eaebc10b0ac1c0154e8265
ip <admin@u00.io>
2026-09-15 10:43:15 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"time"
-
 	"github.com/u00io/nuiforms/examples"
-	"github.com/u00io/nuiforms/ui"
 )
 
-func thPrintAllWidgets() {
-	time.Sleep(1000 * time.Millisecond)
-	for {
-		ui.PrintAllWidgets()
-		time.Sleep(1000 * time.Millisecond)
-	}
-}
-
 func main() {
-	go thPrintAllWidgets()
-	examples.RunSample()
+	examples.Run()
 }
